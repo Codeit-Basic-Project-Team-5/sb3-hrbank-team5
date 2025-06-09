626cff4609ba55498cbe1f625122f724ecd2b2cd
okod <okodee@naver.com>
2025-06-09 14:25:22 +0900

    feat: 응답 형태에 맞게 EmployeeDto 수정
    - 부서 정보 포함
    - 상태 한글로 표시
    - 프로필 이미지 ID 포함(임시)

--- src/main/java/com/ohgiraffers/hrbank/mapper/EmployeeMapper.java
+++ src/main/java/com/ohgiraffers/hrbank/mapper/EmployeeMapper.java
@@ -3,8 +3,14 @@ package com.ohgiraffers.hrbank.mapper;
 import com.ohgiraffers.hrbank.dto.data.EmployeeDto;
 import com.ohgiraffers.hrbank.entity.Employee;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface EmployeeMapper {
+
+    @Mapping(target = "departmentId", source = "department.id")
+    @Mapping(target = "departmentName", source = "department.name")
+    @Mapping(target = "status", source = "status.statusName")
+    @Mapping(target = "profileImageId", constant = "1L")    // 임시로 1L (나중에 실제 프로필 이미지 구현 시 수정)
     EmployeeDto toDto(Employee employee);
 }
