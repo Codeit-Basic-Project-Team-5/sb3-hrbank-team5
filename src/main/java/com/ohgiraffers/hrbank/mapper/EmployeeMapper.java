9d29f02b81a49bd881efee6c8f55af20a8801c17
okod <okodee@naver.com>
2025-06-09 10:17:51 +0900

    feat: MapStruct 사용하여 EmployeeMapper  수정

--- src/main/java/com/ohgiraffers/hrbank/mapper/EmployeeMapper.java
+++ src/main/java/com/ohgiraffers/hrbank/mapper/EmployeeMapper.java
@@ -1,22 +1,10 @@
 package com.ohgiraffers.hrbank.mapper;
 
-import com.ohgiraffers.hrbank.dto.response.EmployeeResponse;
+import com.ohgiraffers.hrbank.dto.data.EmployeeDto;
 import com.ohgiraffers.hrbank.entity.Employee;
+import org.mapstruct.Mapper;
 
-public class EmployeeMapper {
-
-    public static EmployeeResponse toResponse(Employee employee) {
-        return EmployeeResponse.builder()
-            .id(employee.getId())
-            .name(employee.getName())
-            .email(employee.getEmail())
-            .employeeNumber(employee.getEmployeeNumber())
-//            .department(employee.getDepartment())
-            .position(employee.getPosition())
-            .hireDate(employee.getHireDate())
-            .status(employee.getStatus())
-//            .프로필 이미지
-            .build();
-    }
-
+@Mapper(componentModel = "spring")
+public interface EmployeeMapper {
+    EmployeeDto toDto(Employee employee);
 }
