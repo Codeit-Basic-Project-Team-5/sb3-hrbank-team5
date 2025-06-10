30d0b58eeeadc9ce01439277e06850d9affe5f8f
Im-Ubin <yuvin3493@gmail.com>
2025-06-10 15:32:34 +0900

    chore: 프론트 파일 추적 제거

--- project-hrbank-fe-1.0.3/src/components/backup/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as BackupTable } from './BackupTable.tsx';
-export { default as BackupToolbar } from './BackupToolbar.tsx';
--- project-hrbank-fe-1.0.3/src/components/dashboard/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as EmployeeDistributionChart } from './EmployeeDistributionChart.tsx';
-export { default as EmployeeStatusCard } from './EmployeeStatusCard.tsx';
-export { default as EmployeeTrendChart } from './EmployeeTrendChart.tsx';
